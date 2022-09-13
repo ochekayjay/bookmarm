0fd0ac071003785b7a32d5fab9106859f64ded91
Dickson Joseph <ochekayjay@gmail.com>
2022-09-13 15:15:08 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
     bodyParser.json()
   );
 
-  app.use(express.static(`${__dirname}/../public`))
+  //app.use(express.static(`${__dirname}/../public`))
 
   app.use(cors({origin: '*'}))
 
